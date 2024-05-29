23ade1c96961d125220cae14038e490d282cd0c7
Zidan Abraham <zidangit07@gmail.com>
2024-05-29 09:28:19 +0800

    db: add migration to create merchants table

---
+++ db/migrations/20240529003103_create_merchants_table.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS merchants;
+DROP TYPE IF EXISTS merchant_categories;
+DROP EXTENSION postgis CASCADE;
---
+++ db/migrations/20240529003103_create_merchants_table.up.sql
@@ -0,0 +1,25 @@
+CREATE EXTENSION postgis;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'merchant_categories') THEN
+    CREATE TYPE merchant_categories AS ENUM (
+      'SmallRestaurant',
+      'MediumRestaurant',
+      'LargeRestaurant',
+      'MerchandiseRestaurant',
+      'BoothKiosk',
+      'ConvenienceStore'
+    );
+  END IF;
+END $$;
+
+CREATE TABLE IF NOT EXISTS merchants (
+  id VARCHAR(26) PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  category merchant_categories NULL,
+  image_url TEXT NOT NULL,
+  location GEOGRAPHY(Point, 4326) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
